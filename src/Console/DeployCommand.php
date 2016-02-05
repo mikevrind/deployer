1c4fa444915ed0982af986e36b00fbf20e4507da
Mike de Vrind <mike@u-lab.nl>
2016-02-05 11:41:46 +0100

    Fix formatting and typo in docblock

--- src/Console/DeployCommand.php
+++ src/Console/DeployCommand.php
@@ -5,61 +5,61 @@
 	use Illuminate\Console\Command;
 	use MikeVrind\Deployer\Deployer;
 
-class DeployCommand extends Command
-{
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'deployer:deploy';
-
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Manually execute the deplot commands without a webhook';
-
-	/**
-	 * Holds in instance of the actual Deployer
-	 *
-	 * @var Deployer
-	 */
-	protected $deployer;
-
-	/**
-	 * @param Deployer $deployer
-	 */
-	public function __construct(Deployer $deployer)
+	class DeployCommand extends Command
 	{
-		$this->deployer = $deployer;
+		/**
+		 * The name and signature of the console command.
+		 *
+		 * @var string
+		 */
+		protected $signature = 'deployer:deploy';
 
-		parent::__construct();
-	}
+		/**
+		 * The console command description.
+		 *
+		 * @var string
+		 */
+		protected $description = 'Manually execute the deploy commands without a webhook';
 
-	/**
-	 * Execute the deployer
-	 *
-	 * @return void
-	 */
-	public function fire()
-	{
+		/**
+		 * Holds in instance of the actual Deployer
+		 *
+		 * @var Deployer
+		 */
+		protected $deployer;
 
-		if ( $this->confirm('Do you wish to manually run the deployer commands? [y|N]'))
+		/**
+		 * @param Deployer $deployer
+		 */
+		public function __construct( Deployer $deployer )
+		{
+			$this->deployer = $deployer;
+
+			parent::__construct();
+		}
+
+		/**
+		 * Execute the deployer
+		 *
+		 * @return void
+		 */
+		public function fire()
 		{
 
-			if( $this->deployer->deploy() )
+			if( $this->confirm( 'Do you wish to manually run the deployer commands? [y|N]' ) )
 			{
-				$this->info('Deployment successful');
-			}
-			else
-			{
-				$this->error($this->deployer->getErrorMessage());
+
+				if( $this->deployer->deploy() )
+				{
+					$this->info( 'Deployment successful' );
+				}
+				else
+				{
+					$this->error( $this->deployer->getErrorMessage() );
+				}
+
 			}
 
 		}
 
 	}
-
-}
