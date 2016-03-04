27e872160834955f01c578a49d8b2fc30b70fe7e
Mike de Vrind <mike@u-lab.nl>
2016-03-04 11:24:56 +0100

    Fix json response

--- src/Middleware/DeployerMiddleware.php
+++ src/Middleware/DeployerMiddleware.php
@@ -6,63 +6,70 @@ use Illuminate\Contracts\Foundation\Application;
 class DeployerMiddleware
 {
 
-    /**
-     * The Laravel application instance.
-     *
-     * @var \Illuminate\Foundation\Application
-     */
-    protected $app;
+	/**
+	 * The Laravel application instance.
+	 *
+	 * @var \Illuminate\Foundation\Application
+	 */
+	protected $app;
 
-    /**
-     * @param Application $app
-     */
-    public function __construct( Application $app )
-    {
-        $this->app = $app;
-    }
+	/**
+	 * @param Application $app
+	 */
+	public function __construct( Application $app )
+	{
+		$this->app = $app;
+	}
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
-     * @return mixed
-     */
-    public function handle( $request, Closure $next )
-    {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Closure $next
+	 * @return mixed
+	 */
+	public function handle( $request, Closure $next )
+	{
 
-        if ( $this->app['config']->get( 'deployer.enabled' ) )
-        {
+		if( $this->app['config']->get( 'deployer.enabled' ) )
+		{
 
-            # Validate the request
-            if ( $this->isValidRequest($request) ) {
+			# Validate the request
+			if( $this->isValidRequest( $request ) )
+			{
 
-                return $next( $request );
-            }
+				return $next( $request );
+			}
 
-            return response( 'The incoming request was invalid', 401 );
+			return response()->json( [
+				'status'  => 401,
+				'message' => 'The incoming request was invalid'
+			], 401 );
 
-        }
+		}
 
-        return response( 'The deployer is not enabled', 503 );
+		return response()->json( [
+			'status'  => 503,
+			'message' => 'The deployer is not enabled'
+		], 503 );
 
-    }
+	}
 
-    /**
-     * Validate the incoming post request
-     *
-     * @param $request
-     * @return bool
-     */
-    private function isValidRequest($request)
-    {
+	/**
+	 * Validate the incoming post request
+	 *
+	 * @param $request
+	 * @return bool
+	 */
+	private function isValidRequest( $request )
+	{
 
-        return
+		return
 
-            $request->input('ref') == 'refs/heads/'.$this->app['config']->get( 'deployer.repository.branch' ) and
-            $request->input('project_id') == $this->app['config']->get( 'deployer.repository.project_id' ) and
-            $request->input('repository.url') == $this->app['config']->get( 'deployer.repository.repository' );
+			$request->input( 'ref' ) == 'refs/heads/' . $this->app['config']->get( 'deployer.repository.branch' ) and
+			$request->input( 'project_id' ) == $this->app['config']->get( 'deployer.repository.project_id' ) and
+			$request->input( 'repository.url' ) == $this->app['config']->get( 'deployer.repository.repository' );
 
-    }
+	}
 
 }
